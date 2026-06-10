06a44bfe1df566845e1cc2b90f4329bbf9a17032
unknown <samimarsh44@gmail.com>
2026-06-10 23:45:49 +0330

    adding cors headers

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.routers import (
     homepage, single, search,
     auth, user, saved, reviews
 )
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +14,17 @@ async def lifespan(app: FastAPI):
     await disconnect_db()
 
 app = FastAPI(title="Movie Database API", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(homepage.router)
 app.include_router(single.router)
